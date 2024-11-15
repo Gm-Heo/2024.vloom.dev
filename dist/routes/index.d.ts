f58fa7b4b89030b085de33c25f2285c143020aa9
Gm-Heo <rodeck826@vloom.dev>
2024-11-15 10:37:00 +0900

    remove dist

--- dist/routes/index.d.ts
+++
@@ -1,3 +0,0 @@
-import { aftBuilder } from "afts/dist/main/aft-builder";
-import { aftRouter } from "afts/dist/main/plugin/aft-router";
-export declare const AppRoutes: (builder: aftBuilder, router: aftRouter) => void;
