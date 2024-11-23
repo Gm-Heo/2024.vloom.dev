8daa45fab8b46ce4b81b2aba1b7d37439209f131
Gm-Heo <rodeck826@vloom.dev>
2024-11-23 16:49:47 +0900

    포트폴리오 서식화

--- src/routes/project/index.ts
+++ src/routes/project/index.ts
@@ -22,8 +22,93 @@ export class ProjectRoute implements Route{
         //nothing to do 
     }
 }
-
+/**
+ * 
+ * @param route 
+ * @param builder 
+ * @returns 
+ */
 const projectPage = (route : Route,builder : aftBuilder) : RouteType => {
+    /**
+     * 프로젝트 페이지를 초기화합니다.
+     * @param element 
+     */
+    const _init = async (element : HTMLElement)=>{
+        await _loadData(element);
+    }
+    /**
+     * 
+     * @param element 
+     */
+    const _loadData = async (element : HTMLElement)=>{
+        const data = await fetch('/assets/portfolio.json')
+        .then(response => response.json())
+        .then(data => {
+            let lang = builder.querystring('lang')||'en';
+            if(['ko','en'].indexOf(lang) == -1){
+                lang = 'en';
+            }
+            _createProject(element,data.projects||[],lang);
+        });
+    }
+    /**
+     * 프로젝트 데이터를 생성합니다.    
+     * @param element 
+     * @param projects 
+     * @param lang 
+     */
+    const _createProject = (element : HTMLElement,projects : any,lang:string='en')=>{
+            
+        const projectsContainer = element.querySelector('.projects .line')!;
+        projects.forEach(project => {
+            const item = document.createElement('div');
+            item.className = 'item';
+            if(!project.preview){
+                project.preview = {'ko':[],'en':[]};
+            }
+            if(!project.client){
+                project.client = {'ko':'', 'en':''};
+            }
+            item.innerHTML = `
+                <div class="date">
+                    <span class="data">${project.date[lang]}</span>
+                </div>
+                <h3 class="title">${project.title[lang]}</h3>
+                <div class="row">
+                    <span class="col">From</span>
+                    <span class="data">${project.from[lang]}</span>
+                </div>
+                <div class="row">
+                    <span class="col">Client</span>
+                    <span class="data">${project.client[lang]}</span>
+                </div>
+                <div class="row">
+                    <span class="col">Role</span>
+                    <span class="data">${project.role[lang]}</span>
+                </div>
+                <div class="desc">
+                    ${project.description[lang].map(desc => `• ${desc}<br>`).join('')}
+                </div>
+                <ul class="tech-list">
+                    ${project.tech.map(tech => `<li>${tech}</li>`).join('')}
+                </ul>
+                <div class="preview-wrap">
+                    <ul class="preview-list">
+                        ${(project.preview||[])[lang].map(item => `
+                            <li data-list="${item.type}" data-src="${item.src}" aft-event="onPreview">
+                                <span style="background-image: url('${item.image}');"></span>
+                            </li>
+                        `).join('')}
+                    </ul>
+                </div>
+            `;
+            projectsContainer.appendChild(item);
+        });
+    }
+    /**
+     * 팝업창을 닫습니다.
+     * @param popup 
+     */
     const _closeBtn = (popup : HTMLElement)=>{
         const btn = document.createElement('button');
         btn.innerHTML = 'X';
@@ -37,6 +122,10 @@ const projectPage = (route : Route,builder : aftBuilder) : RouteType => {
         });
         popup.appendChild(btn);
     }
+    /**
+     * 비디오 팝업창을 생성합니다.
+     * @param src 
+     */
     const _createVideoPopup = (src: string) => {
         
         const popup = document.createElement('div');
@@ -73,6 +162,10 @@ const projectPage = (route : Route,builder : aftBuilder) : RouteType => {
         document.body.appendChild(popup);
 
     }
+    /**
+     * 이미지 팝업창을 생성합니다.
+     * @param src 
+     */
     const _createImagePopup = (src: string) => {
         const popup = document.createElement('div');
         popup.style.cssText = `
@@ -106,6 +199,11 @@ const projectPage = (route : Route,builder : aftBuilder) : RouteType => {
         _closeBtn(popup);
         document.body.appendChild(popup);
     }
+    /**
+     * 프로젝트 미리보기를 처리합니다.
+     * @param btn 
+     * @param e 
+     */
     const _onPreview = (btn : HTMLElement,e:MouseEvent)=>{
         const src = btn.getAttribute('data-src');
         const type = btn.getAttribute('data-list');
@@ -121,7 +219,8 @@ const projectPage = (route : Route,builder : aftBuilder) : RouteType => {
         event:()=>{
             return {
                 path:'project/main',
-                onload:(element)=>{
+                onload:async (element)=>{
+                    await   _init(element);
                     return element;
                 },
                 event:{
